5d164fd4ab12bfc2578503d4e4882d10b91471aa
Darrick <darrickedbert1@gmail.com>
2021-04-03 18:02:58 +0700

    added index.html and modified python files to support it

---
+++ test.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class ToDo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100)) #Max characters we can type in is 100
+    complete = db.Column(db.Boolean)
+
+# Home Page
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/')
+def index():
+    """
+    Index will change later.
+    """ 
+    #show all todos
+    todo_list = ToDo.query.all()
+    print(todo_list)
+    return render_template('base.html', todo_list=todo_list)
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    #add new To-Do
+    title = request.form.get("title")
+    new_todo = ToDo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    todo = ToDo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = ToDo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+@app.route('/about')
+def about():
+    """
+    Defining an about webpage separate from our index page.
+    """
+    return "A To-Do web app. Hopefully."
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
